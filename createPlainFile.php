60de4ebe913dc200723e65c258fb0df0756be986
autillo <angel.redondo.garcia@gmail.com>
2019-06-02 20:02:16 +0100

    create csv

--- createPlainFile.php
+++ createPlainFile.php
@@ -1,11 +1,10 @@
 <?php
 
-$string0 = 'hi there';
-$string1 = 'how are you?';
-$string2 = 'end';
+//__DIR__ full path from the /
+// without __DIR__ we are in the file folder
 
+//strings
 $file = fopen(__DIR__.'/../../downloads/plainFile.txt', "w") or die("Unable to open file!");
-fputs($file, "hi there:\n");
-fputs($file, "hi there:\n");
+fputs($file, "string here\n");
 fputs($file, "hi there:\n");
 fclose($file);
