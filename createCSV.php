2de1268b45b4141235e7026b51e9ab20005fab96
autillo <angel.redondo.garcia@gmail.com>
2019-06-02 20:21:24 +0100

    testing

--- createCSV.php
+++ createCSV.php
@@ -3,7 +3,7 @@
 $header = ['date', 'location', 'event'];
 
 //arrays
-$file = fopen(__DIR__.'/../../downloads/test.csv', "w");
+$file = fopen(__DIR__.'/../../downloads/test.csv', "w") or die("Unable to open file!");
 fputcsv($file, $header);
 fputcsv($file, ['15-04-1978', 'madrid', 'born']);
 fclose($file);
---
+++ createWritePlainFile.php
@@ -0,0 +1,20 @@
+<?php
+
+//__DIR__ full path from the /
+// without __DIR__ we are in the file folder
+
+//strings
+$file = fopen(__DIR__.'/../../downloads/plainFile.txt', "w")
+fputs($file, "string here\n");
+fputs($file, "hi there:\n");
+fclose($file);
+
+// "a"
+// Open a file for write only. The existing data in file is preserved. 
+// File pointer starts at the end of the file. 
+// Creates a new file if the file doesn't exist
+
+$file = fopen(__DIR__.'/../../downloads/plainFile.txt', "a")
+fputs($file, "\n");
+fputs($file, "added later.");
+fclose($file);
